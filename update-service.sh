c4a81f32dc82deaca143cdf00926efbd5c86e861
totto <totto@totto.org>
2017-04-03 13:19:03 +0200

    upgraded update-service.sh script

--- update-service.sh
+++ update-service.sh
@@ -1,30 +1,42 @@
-#!/bin/bash
+#!/bin/sh
+# Script to download deployment unit from a Maven artifact repository.
 
-A=Whydah-StatisticsService
+releaseRepo=https://mvnrepo.cantara.no/content/repositories/releases
+snapshotRepo=https://mvnrepo.cantara.no/content/repositories/snapshots
+groupId=net/whydah/service
+artifactId=Whydah-StatisticsService
 V=SNAPSHOT
 
 
 if [[ $V == *SNAPSHOT* ]]; then
-   echo Note: If the artifact version contains "SNAPSHOT" - the artifact latest greatest snapshot is downloaded, Irrelevent of version number!!!
-   path="http://mvnrepo.cantara.no/content/repositories/snapshots/net/whydah/statistics/$A"
-   version=`curl -s "$path/maven-metadata.xml" | grep "<version>" | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | tail -n 1`
-   echo "Version $version"
+   echo Note: If the artifact version contains "SNAPSHOT", the latest snapshot version is downloaded, ignoring the version before SNAPSHOT.
+   path="$snapshotRepo/$groupId/$artifactId"
+   version=`curl -s "$path/maven-metadata.xml" | grep "<version>"  | grep "$V" | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | tail -n 1`
+   #echo "version=$version, path=$path"
    build=`curl -s "$path/$version/maven-metadata.xml" | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-   JARFILE="$A-$build.jar"
+   JARFILE="$artifactId-$build.jar"
    url="$path/$version/$JARFILE"
 else #A specific Release version
-   path="http://mvnrepo.cantara.no/content/repositories/snapshots/net/whydah/statistics/$A"
-   url=$path/$V/$A-$V.jar
-   JARFILE=$A-$V.jar
+   path="$releaseRepo/$groupId/$artifactId"
+   url=$path/$V/$artifactId-$V.jar
+   JARFILE=$artifactId-$V.jar
 fi
 
-# Download
+# Download artifact
 echo Downloading $url
 wget -O $JARFILE -q -N $url
 
 
-#Create symlink or replace existing sym link
-if [ -h $A.jar ]; then
-   unlink $A.jar
+# Create symlink or replace existing sym link
+if [ -h $artifactId.jar ]; then
+   unlink $artifactId.jar
 fi
-ln -s $JARFILE $A.jar
+ln -s $JARFILE $artifactId.jar
+
+# Delete old jar files
+jar=$artifactId*.jar
+nrOfJarFilesToDelete=`ls $jar -A1t | tail -n +6 | wc -l`
+if [[ $nrOfJarFilesToDelete > 0 ]]; then
+    echo Deleting $nrOfJarFilesToDelete old jar files. Keep the 4 newest + the symlink.
+    ls $jar -A1t | tail -n +6 | xargs rm
+fi
